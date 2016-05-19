7481d2e6d1e972ca8ed048362303573c322f62aa
Zoe Ames <zoeames@gmail.com>
2016-05-19 14:08:41 -0500

    added gulp

--- app/app.js
+++ app/app.js
@@ -1,10 +1,10 @@
 var app = angular.module("TodoApp", []);
 
 app.controller("NavCtrl", function($scope){
-	$scope.navItems = [{name: "Logout"}, {name: "All Items"}, {name:"New Item"}]
-})
+	$scope.navItems = [{name: "Logout"}, {name: "All Items"}, {name:"New Item"}];
+});
 
 app.controller("TodoCtrl", function($scope){
 	$scope.welcome = "hello";
 
-})
+});

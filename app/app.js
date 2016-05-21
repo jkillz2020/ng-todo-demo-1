8f85928b6db3c1c8393549154aa22b3c27deb0d8
Zoe Ames <zoeames@gmail.com>
2016-05-21 13:28:38 -0500

    added routes

--- app/app.js
+++ app/app.js
@@ -1,1 +1,18 @@
-var app = angular.module("TodoApp", []);
+var app = angular.module("TodoApp", ["ngRoute"]);
+
+app.config(function($routeProvider){
+  $routeProvider.
+    when('/items/list',{
+      templateUrl: 'partials/item-list.html',
+      controller: 'TodoCtrl'
+    }).
+    when('/items/new', {
+      templateUrl: 'partials/item-new.html',
+      controller: 'TodoCtrl'
+    }).
+    when('/items/details', {
+      templateUrl: 'partials/item-details.html',
+      controller: "TodoCtrl"
+    }).
+    otherwise('/items/list');
+});

fd7e029018aecad224bd4ef23cdb1bc837bcb0d5
Zoe Ames <zoeames@gmail.com>
2016-05-21 10:36:30 -0500

    added form fields

--- app/app.js
+++ app/app.js
@@ -6,8 +6,42 @@ app.controller("NavCtrl", function($scope){
 
 app.controller("TodoCtrl", function($scope){
 	$scope.welcome = "hello";
-      $scope.showListView = true;
+      $scope.showListView = false;
+      $scope.newTask = {};
       
+      $scope.items = [
+        {
+          id: 0,
+          task: "mow the lawn",
+          isCompleted: true,
+          dueDate: "12/5/17",
+          assignedTo: "greg",
+          location: "Zoe's house",
+          urgency: "low",
+          dependencies: ["sunshine", "clippers", "hat", "water", "headphones"]
+        },
+        {
+          id: 1,
+          task: "grade quizzes",
+          isCompleted: false,
+          dueDate: "12/5/15",
+          assignedTo: "Joe",
+          location: "NSS",
+          urgency: "high",
+          dependencies: ["wifi", "tissues", "vodka"]
+        },
+        {
+          id: 2,
+          task: "take a nap",
+          isCompleted: false,
+          dueDate: "5/21/16",
+          assignedTo: "zoe",
+          location: "Zoe's house",
+          urgency: "medium",
+          dependencies: ["hammock", "cat", "pillow", "blanket"]
+        }
+      ];
+
       $scope.newItem = function(){
           console.log("you clicked new Item");
           $scope.showListView = false;

48d1fc8271ef95fe9dd925cbbbe2cc53d9440701
Zoe Ames <zoeames@gmail.com>
2016-05-21 10:12:35 -0500

    added nav buttons

--- app/app.js
+++ app/app.js
@@ -6,5 +6,17 @@ app.controller("NavCtrl", function($scope){
 
 app.controller("TodoCtrl", function($scope){
 	$scope.welcome = "hello";
+      $scope.showListView = true;
+      
+      $scope.newItem = function(){
+          console.log("you clicked new Item");
+          $scope.showListView = false;
+      };
+      $scope.allItem = function(){
+        console.log("you clicked all items");
+        $scope.showListView = true;
+      };
+
+
 
 });

969b7b8f81bd58f3f13aa210db63b9d524adcb3c
Zoe Ames <zoeames@gmail.com>
2016-05-23 18:23:23 -0500

    cleaned up files

--- app/controllers/ItemListCtrl.js
+++ app/controllers/ItemListCtrl.js
@@ -1,34 +1,3 @@
 app.controller("ItemListCtrl", function($scope){
-      $scope.items = [
-        {
-          id: 0,
-          task: "mow the lawn",
-          isCompleted: true,
-          dueDate: "12/5/17",
-          assignedTo: "greg",
-          location: "Zoe's house",
-          urgency: "low",
-          dependencies: "sunshine, clippers, hat, water, headphones"
-        },
-        {
-          id: 1,
-          task: "grade quizzes",
-          isCompleted: false,
-          dueDate: "12/5/15",
-          assignedTo: "Joe",
-          location: "NSS",
-          urgency: "high",
-          dependencies: "wifi, tissues, vodka"
-        },
-        {
-          id: 2,
-          task: "take a nap",
-          isCompleted: false,
-          dueDate: "5/21/16",
-          assignedTo: "zoe",
-          location: "Zoe's house",
-          urgency: "medium",
-          dependencies: "hammock, cat, pillow, blanket"
-        }
-      ];
+      $scope.items = [];
 });

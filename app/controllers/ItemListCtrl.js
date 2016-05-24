32028f6023a1e3979b74f82b6c78128f48b030ef
Zoe Ames <zoeames@gmail.com>
2016-05-23 21:17:10 -0500

    added factory

--- app/controllers/ItemListCtrl.js
+++ app/controllers/ItemListCtrl.js
@@ -1,16 +1,25 @@
-app.controller("ItemListCtrl", function($scope, $http, $location){
+app.controller("ItemListCtrl", function($scope, $http, $location, itemStorage){
       $scope.items = [];
-      var getItems = function(){
-        $http.get("https://todo-appz2.firebaseio.com/items.json")
-          .success(function(itemObject){
-            var itemCollection = itemObject;
-            Object.keys(itemCollection).forEach(function(key){
-              itemCollection[key].id=key;
-              $scope.items.push(itemCollection[key]);
-            })
-          });
-      }
-      getItems();
+      // var getItems = function(){
+      //   $http.get("https://todo-appz2.firebaseio.com/items.json")
+      //     .success(function(itemObject){
+      //       var itemCollection = itemObject;
+      //       Object.keys(itemCollection).forEach(function(key){
+      //         itemCollection[key].id=key;
+      //         $scope.items.push(itemCollection[key]);
+      //       })
+      //     });
+      // }
+      // getItems();
+
+      itemStorage.getItemList().then(function(itemCollection){
+        console.log("itemCollection from promise", itemCollection);
+        $scope.items = itemCollection;
+      })
+
+
+
+
         $scope.itemDelete = function(itemId){
           console.log("itemId", itemId);
           $http

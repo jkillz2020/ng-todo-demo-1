760362332ffa511c79ed763ad64dfbb957ccda61
Zoe Ames <zoeames@gmail.com>
2016-05-23 20:52:17 -0500

    added details page

--- app/controllers/ItemListCtrl.js
+++ app/controllers/ItemListCtrl.js
@@ -1,13 +1,25 @@
-app.controller("ItemListCtrl", function($scope, $http){
+app.controller("ItemListCtrl", function($scope, $http, $location){
       $scope.items = [];
-
-      $http.get("https://todo-appz2.firebaseio.com/items.json")
-        .success(function(itemObject){
-          var itemCollection = itemObject;
-          Object.keys(itemCollection).forEach(function(key){
-            itemCollection[key].id=key;
-            $scope.items.push(itemCollection[key]);
-          })
-        })
+      var getItems = function(){
+        $http.get("https://todo-appz2.firebaseio.com/items.json")
+          .success(function(itemObject){
+            var itemCollection = itemObject;
+            Object.keys(itemCollection).forEach(function(key){
+              itemCollection[key].id=key;
+              $scope.items.push(itemCollection[key]);
+            })
+          });
+      }
+      getItems();
+        $scope.itemDelete = function(itemId){
+          console.log("itemId", itemId);
+          $http
+            .delete(`https://todo-appz2.firebaseio.com/items/${itemId}.json`)
+            .success(function(response){
+              console.log(response);
+              $scope.items = [];
+              getItems();
+            })
+        }
 
 });

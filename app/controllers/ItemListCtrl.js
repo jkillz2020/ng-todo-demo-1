9272c7724e83f7e9a72cfe95a4fa9831fc311265
Zoe Ames <zoeames@gmail.com>
2016-05-23 19:18:34 -0500

    added get and post to /items.json

--- app/controllers/ItemListCtrl.js
+++ app/controllers/ItemListCtrl.js
@@ -1,3 +1,13 @@
-app.controller("ItemListCtrl", function($scope){
+app.controller("ItemListCtrl", function($scope, $http){
       $scope.items = [];
+
+      $http.get("https://todo-appz2.firebaseio.com/items.json")
+        .success(function(itemObject){
+          var itemCollection = itemObject;
+          Object.keys(itemCollection).forEach(function(key){
+            itemCollection[key].id=key;
+            $scope.items.push(itemCollection[key]);
+          })
+        })
+
 });

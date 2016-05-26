6d3928dd8f09af06186c068acd5056e2fd7375b8
Zoe Ames <zoeames@gmail.com>
2016-05-25 21:02:51 -0500

    edit working

---
+++ app/controllers/ItemEditCtrl.js
@@ -0,0 +1,16 @@
+app.controller("ItemEditCtrl", function($scope, $location, $routeParams, itemStorage){
+    $scope.newTask = {};
+
+    itemStorage.getSingleItem($routeParams.itemId)
+        .then(function successCallback(response){
+            $scope.newTask=response;
+        })
+      
+    $scope.addNewItem = function(){
+        itemStorage.updateItem($routeParams.itemId, $scope.newTask)
+            .then(function successCallback(response) {
+                console.log(response);
+                $location.url("/items/list");
+            });
+    };
+});
